306f294a3b4ab26344dada262c007f466ed22b4f
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-11-14 18:41:11 +0100

    Implement AJAX GET request to food2fork API

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,22 @@
 
 import axios from 'axios';
 
+
 async function getResults(query) {
-  
+  const API_KEY = '452bd0b5e11b1a34c11e392ab1d170c4';
+  const PROXY = 'https://cors-anywhere.herokuapp.com/';
+
+  try {
+    const res = await axios(`${PROXY}https://www.food2fork.com/api/search?key=${API_KEY}&q=${query}`);
+    const recipes = res.data.recipes;
+  } catch(err) {
+    alert(err);
+  }
+
+  return recipes;
 }
 
-getResults(); 
+getResults('chicken')
+  .then(recipes => {
+    console.log(recipes);
+  });
