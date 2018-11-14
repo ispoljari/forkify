341b4b6dcde9b41984f4eec9118f23201da2176d
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-11-14 17:23:33 +0100

    Install axios

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+// API Key: 452bd0b5e11b1a34c11e392ab1d170c4 
+// URL endpoint: https://www.food2fork.com/api/search
+
+import axios from 'axios';
+
+async function getResults(query) {
+  
+}
+
+getResults(); 
