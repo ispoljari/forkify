b8e3b64f721db572f0aad1d5c1ac1649182e7bf8
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-11-17 12:28:00 +0100

    Start building the Recipe model

---
+++ src/js/models/model.recipe.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export default class Recipe {
+  constructor(id) {
+    this.id = id;
+  }
+
+  async getRecipe() {
+    try {
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
